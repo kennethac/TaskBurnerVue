3f0a53b100d530b4429ccf740b1b4ee1a6a59284
Kenneth Christensen <1kennethac@gmail.com>
2019-03-08 12:28:16 -0700

    Home also works with router link (obviously pretty inefficiently with the API firing everytime, sometimes multiple times.

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -16,10 +16,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
+            <router-link to="/" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/combined.html">Combined</a>
+            <router-link to="/" class="nav-link">Combined</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/class/english" class="nav-link">English</router-link>
